ff7a581d471d1ecbe60a1f21d653d15f949f35c3
Valeriya Pavlova <vla.valeriya@gmail.com>
2023-08-11 14:41:42 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Heading } from './App.styled';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Heading>Phonebook</Heading>
+      <ContactForm />
+      <Heading>Contacts</Heading>
+       <Filter /> 
+      <ContactList />
     </div>
   );
 };
---
+++ src/redux/ContactSlice/selectors.js
@@ -0,0 +1,2 @@
+export const ContactSelector = state => state.contact.contacts.items;
+export const FilterSelector = state => state.contact.contacts.filter;
